7b1cb0bd025792f3640ae57d52172d8465d2d42a
DoTienOanh <164726088+DoTienOanh@users.noreply.github.com>
2024-05-22 15:06:38 +0700

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from langchain.llms import OpenAI
-st.title(' Hệ thống sinh đề kiểm tra tại Học viện Ngân hàng')
-text_input = st.text_area( label = 'Nhập prompt sinh câu hỏi: ')
-
-button = st.button("Submit", key="button")
-if button:
-  completion = 
